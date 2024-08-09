5c428de892a36a777ab68a3202988185147b73a5
Kenneth Muturi <muturiken@gmail.com>
2024-08-09 16:22:13 +0300

    gasheri hospital

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
-import Todos from "./components/Todos";
+import Hospital from "./components/Hospital";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <div className="container-fluid">
         <section className="mt-3 p-3">
           <div className="d-flex justify-content-center">
-            <Todos />
+            <Hospital />
           </div>
         </section>
       </div>
