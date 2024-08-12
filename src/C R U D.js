71f98a5baa7fb64efac8da207e618322a668dcb9
Kenneth Muturi <muturiken@gmail.com>
2024-08-12 11:15:47 +0300

    gasheri hospital

--- src/C R U D.js
+++ src/C R U D.js
@@ -1,9 +1,9 @@
 C R U D
 
-Reading => get database, [json, objects], putting on html(php, ejs, reactjs)
-Creating => [json, Object], convert to sql(php, js), database
-updating => [json, Object], convert to sql(php, js), database
-delete => [id, sting, array], convert to sql(php, js), database
+Reading = get database, [json, objects], putting on html(php, ejs, reactjs)
+Creating = [json, Object], convert to sql(php, js), database
+updating = [json, Object], convert to sql(php, js), database
+delete =[id, sting, array], convert to sql(php, js), database
 
 
 Fullstack
